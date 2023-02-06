557261dd5eb1d0ac45673eb853e4e98b3e4f0061
Aleksey Nebesniy <103752883+ACID-Sky@users.noreply.github.com>
2023-02-06 16:26:40 +0300

    Убрал закоментированный блок

--- Navigation_Nebesniy/Profile line/Photos/PhotoData.swift
+++ Navigation_Nebesniy/Profile line/Photos/PhotoData.swift
@@ -7,29 +7,6 @@
 
 import UIKit
 
-//var photos: [String] = [
-//    "images",
-//    "images-9",
-//    "images-19",
-//    "images-13",
-//    "images-4",
-//    "images-5",
-//    "images-6",
-//    "images-7",
-//    "images-8",
-//    "images-1",
-//    "images-10",
-//    "images-11",
-//    "images-12",
-//    "images-3",
-//    "images-14",
-//    "images-15",
-//    "images-16",
-//    "images-17",
-//    "images-18",
-//    "images-2",
-//    "images-20"
-//    ]
 var photos: [UIImage] = [
             UIImage(named: "images")!,
             UIImage(named: "images-9")!,
