52f0b7f82ecf507ff4c9504f6d8abb51924599b1
Aleksey Nebesniy <103752883+ACID-Sky@users.noreply.github.com>
2023-02-19 15:31:30 +0300

    Поправил вид плеера по рекомендациям преподавателя

--- Navigation_Nebesniy/Feed line/MusicView/MusicViewController.swift
+++ Navigation_Nebesniy/Feed line/MusicView/MusicViewController.swift
@@ -11,37 +11,70 @@ import AVFoundation
 class MusicViewController: UIViewController {
 
     var Player = AVAudioPlayer()
+    private lazy var coverSong = UIImageView()
     private lazy var trackNameLabel = UILabel()
+    private lazy var stackForButton = UIStackView()
     private lazy var playtButton = UIButton()
     private lazy var stopButton = UIButton()
     private lazy var nextButton = UIButton()
     private lazy var previousButton = UIButton()
-    private let playList: [String] = ["Queen", "Noize_MC_-_Vsjo_kak_u_lyudejj", "maks_korzh_-_amsterdam", "lone_-_ey_bro", "Каста - Выходи Гулять"]
     private lazy var nomberOfTrack: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .systemBackground
+        setupCoverSong()
         setuptrackNameLabel()
+        setupStackForButton()
         setupPlayButton()
         setupStopButton()
-        setupNextButton()
         setupPreviousButton()
+        setupNextButton()
         playSound(index: nomberOfTrack)
+    }
+
+    private func setupCoverSong() {
+        coverSong.translatesAutoresizingMaskIntoConstraints = false
+        coverSong.clipsToBounds = true
+
+        self.view.addSubview(coverSong)
+
+        NSLayoutConstraint.activate([
+            self.coverSong.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.coverSong.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: -50),
+            self.coverSong.heightAnchor.constraint(equalToConstant: 200),
+            self.coverSong.widthAnchor.constraint(equalToConstant: 200),
+            ])
     }
 
     private func setuptrackNameLabel() {
         trackNameLabel.translatesAutoresizingMaskIntoConstraints = false
         trackNameLabel.numberOfLines = 1
-        trackNameLabel.textAlignment = .left
+        trackNameLabel.textAlignment = .center
 
         self.view.addSubview(trackNameLabel)
 
         NSLayoutConstraint.activate([
-            self.trackNameLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            self.trackNameLabel.topAnchor.constraint(equalTo: self.coverSong.bottomAnchor, constant: 16),
             self.trackNameLabel.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
             self.trackNameLabel.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
-            self.trackNameLabel.heightAnchor.constraint(equalToConstant: 32)
+            self.trackNameLabel.heightAnchor.constraint(equalToConstant: 32),
+            ])
+    }
+
+    private func setupStackForButton() {
+        stackForButton.axis = .horizontal
+        stackForButton.spacing = 16
+        stackForButton.distribution = .fillEqually
+        stackForButton.clipsToBounds = true
+        stackForButton.translatesAutoresizingMaskIntoConstraints = false
+
+        self.view.addSubview(stackForButton)
+
+        NSLayoutConstraint.activate([
+            self.stackForButton.topAnchor.constraint(equalTo: self.trackNameLabel.bottomAnchor, constant: 16),
+            self.stackForButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.stackForButton.heightAnchor.constraint(equalToConstant: 20),
                 ])
     }
 
@@ -50,14 +83,7 @@ class MusicViewController: UIViewController {
         playtButton.translatesAutoresizingMaskIntoConstraints = false
         playtButton.addTarget(self, action:  #selector(playButtonDidTap), for: .touchUpInside)
 
-        self.view.addSubview(playtButton)
-
-        NSLayoutConstraint.activate([
-            self.playtButton.topAnchor.constraint(equalTo: self.trackNameLabel.bottomAnchor, constant: 16),
-            self.playtButton.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
-            self.playtButton.heightAnchor.constraint(equalToConstant: 20),
-            self.playtButton.widthAnchor.constraint(equalToConstant: 20)
-        ])
+        self.stackForButton.addArrangedSubview(playtButton)
     }
 
     private func setupStopButton() {
@@ -65,51 +91,31 @@ class MusicViewController: UIViewController {
         stopButton.translatesAutoresizingMaskIntoConstraints = false
         stopButton.addTarget(self, action:  #selector(stopButtonDidTap), for: .touchUpInside)
 
-        self.view.addSubview(stopButton)
-
-        NSLayoutConstraint.activate([
-            self.stopButton.topAnchor.constraint(equalTo: self.trackNameLabel.bottomAnchor, constant: 16),
-            self.stopButton.leadingAnchor.constraint(equalTo: playtButton.trailingAnchor, constant: 16),
-            self.stopButton.heightAnchor.constraint(equalToConstant: 20),
-            self.stopButton.widthAnchor.constraint(equalToConstant: 20)
-        ])
+        self.stackForButton.addArrangedSubview(stopButton)
     }
 
-    private func setupNextButton() {
+    private func setupPreviousButton() {
         previousButton.setImage(UIImage(systemName: "backward.fill"), for: .normal)
         previousButton.translatesAutoresizingMaskIntoConstraints = false
         previousButton.addTarget(self, action:  #selector(previousButtonDidTap), for: .touchUpInside)
 
-        self.view.addSubview(previousButton)
-
-        NSLayoutConstraint.activate([
-            self.previousButton.topAnchor.constraint(equalTo: self.trackNameLabel.bottomAnchor, constant: 16),
-            self.previousButton.leadingAnchor.constraint(equalTo: stopButton.trailingAnchor, constant: 16),
-            self.previousButton.heightAnchor.constraint(equalToConstant: 20),
-            self.previousButton.widthAnchor.constraint(equalToConstant: 20)
-        ])
+        self.stackForButton.addArrangedSubview(previousButton)
     }
 
-    private func setupPreviousButton() {
+    private func setupNextButton() {
         nextButton.setImage(UIImage(systemName: "forward.fill"), for: .normal)
         nextButton.translatesAutoresizingMaskIntoConstraints = false
         nextButton.addTarget(self, action:  #selector(nextButtonDidTap), for: .touchUpInside)
 
-        self.view.addSubview(nextButton)
-
-        NSLayoutConstraint.activate([
-            self.nextButton.topAnchor.constraint(equalTo: self.trackNameLabel.bottomAnchor, constant: 16),
-            self.nextButton.leadingAnchor.constraint(equalTo: previousButton.trailingAnchor, constant: 16),
-            self.nextButton.heightAnchor.constraint(equalToConstant: 20),
-            self.nextButton.widthAnchor.constraint(equalToConstant: 20)
-        ])
+        self.stackForButton.addArrangedSubview(nextButton)
     }
 
-    private func playSound( index: Int) {
+    private func playSound(index: Int) {
         do {
-            Player = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: playList[index], ofType: "mp3")!))
+            Player = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: Music[index].name, ofType: "mp3")!))
             Player.prepareToPlay()
-            trackNameLabel.text = playList[index]
+            trackNameLabel.text = Music[index].text
+            coverSong.image = Music[index].image
         }
         catch {
             print(error)
@@ -139,7 +145,7 @@ class MusicViewController: UIViewController {
     }
 
     @objc private func nextButtonDidTap() {
-        if nomberOfTrack < playList.count - 1 {
+        if nomberOfTrack < Music.count - 1 {
             nomberOfTrack += 1
             playSound(index: nomberOfTrack)
             self.playtButton.setImage(UIImage(systemName: "pause.fill"), for: .normal)
