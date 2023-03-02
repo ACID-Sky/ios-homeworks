7bc0f744555a584f9f1304140827e0ce6e9c9371
Aleksey Nebesniy <103752883+ACID-Sky@users.noreply.github.com>
2023-03-02 13:11:13 +0300

    Выполнил задание 1 Домашнее задание к занятию 1.2. JSON. Codable. Модели данных

--- Navigation_Nebesniy/Feed line/Post/Info/InfoViewController.swift
+++ Navigation_Nebesniy/Feed line/Post/Info/InfoViewController.swift
@@ -19,16 +19,73 @@ class InfoViewController: UIViewController {
         return button
     }()
 
+    private lazy var jsonTitleLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .systemOrange
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.clipsToBounds = true
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.view.backgroundColor = .systemGray3
         self.view.addSubview(self.button)
+        self.view.addSubview(self.jsonTitleLabel)
+        loadJSON()
+
+        self.jsonTitleLabel.layer.cornerRadius = 10
+        self.jsonTitleLabel.layer.borderWidth = 0.5
+
+        NSLayoutConstraint.activate([
+            self.jsonTitleLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 32),
+            self.jsonTitleLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16),
+            self.jsonTitleLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16),
+            self.jsonTitleLabel.heightAnchor.constraint(equalToConstant: 80),
+        ])
+
 
     }
 
+    private func loadJSON () {
+        if let url = URL(string: "https://jsonplaceholder.typicode.com/todos/5") {
+            let task = URLSession.shared.dataTask(with: url) { data, response, error in
+                
+                if let unwrappedData = data {
+
+                    do {
+                        let dictionary = try JSONSerialization.jsonObject(with: unwrappedData)
+
+                        if let casted = dictionary as? [String: Any] {
+
+                            if let userId = casted["userId"] as? Int,
+                                let id = casted["id"] as? Int ,
+                                let title = casted["title"] as? String ,
+                                let completed = casted["completed"] as? Bool {
+                                let jsonData = FirstJSONData(userId: userId, id: id, title: title, completed: completed)
+
+                                DispatchQueue.main.async {
+                                    self.jsonTitleLabel.text = jsonData.title
+                                }
+                            }
+                        }
+
+                    } catch let error {
+                        print("😱", error)
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+
     @objc private func didTapButton() {
-        let alert = UIAlertController(title: "Do you want to the Hell?", message: "If you really want to the Hell push 'Yes' else push 'No'.", preferredStyle: .actionSheet)
+        let alert = UIAlertController(title: "Do you want to the Hell?",
+                                      message: "If you really want to the Hell push 'Yes' else push 'No'.",
+                                      preferredStyle: .actionSheet)
 
         let yesAction = UIAlertAction(title: "Yes", style: .default) { _ in
             print("User wants to the Hell!")
---
+++ Navigation_Nebesniy/Feed line/Post/Info/JSONData.swift
@@ -0,0 +1,15 @@
+//
+//  JSONData.swift
+//  Navigation_Nebesniy
+//
+//  Created by Лёха Небесный on 02.03.2023.
+//
+
+import Foundation
+
+struct FirstJSONData {
+    let userId: Int
+    let id: Int
+    let title: String
+    let completed: Bool
+}
